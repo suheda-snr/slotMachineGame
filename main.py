b3899f0e22e19166900f273ef22133da06f673b9
suheda-snr <t3sesu00@students.oamk.fi>
2024-12-29 00:00:11 +0200

    Enhance slot machine logic with betting functionality

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ MIN_BET = 1
 def deposit():
     #continually ask user the deposit amount until getting a valid amount
     while True:
-        amount = input("What would you like to deposit? ")
+        amount = input("What would you like to deposit? €")
         # if input is digit convert the default string to int
         if amount.isdigit():
             amount = int(amount)
-            # check the value if greater than 0
-            if amount > 0:
-                break
-            else: 
-                print("Amount must be greater than 0.")
-        except ValueError:
-            print("Please enter a number.")
-         continue
+            try:
+                # check the value if greater than 0
+                if amount > 0:
+                    break
+                else: 
+                    print("Amount must be greater than 0.")
+            except ValueError:
+                print("Please enter a number.")
+            continue
             
     return amount
 
 
-
 def get_number_of_lines():
-       #continually ask user the deposit amount until getting a valid amount
+       #continually ask user the lines until getting a valid number
     while True:
         lines = input("Enter the number of lines to bet on between 1-"+ str(MAX_LINES)+ ". ")
         # if input is digit convert the default string to int
@@ -38,13 +38,41 @@ def get_number_of_lines():
             print("Please enter a number.")
             
     return lines
+
+def get_bet():
+       #continually ask user the bet amount until getting a valid amount
+    while True:
+        amount = input("What would you like to bet on each line? €")
+        # if input is digit convert the default string to int
+        if amount.isdigit():
+            amount = int(amount)
+            try:
+                # check the value if greater than 0
+                if MIN_BET <= amount <= MAX_BET:
+                    break
+                else: 
+                    print(f"Amount must be between {MIN_BET} - {MAX_BET}.")
+            except ValueError:
+                print("Please enter a number.")
+            continue
+            
+    return amount
     
 
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: €{balance}")
+
+        else:
+            break
+
+    print (f"You're betting €{bet} on {lines} lines. Total bet is equal to €{total_bet}")
 
 main()
-
